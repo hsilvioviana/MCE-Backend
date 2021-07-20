fe54a9594ffff8ba3e0cf004a9c506de5b2fe517
hsilvioviana <hsilvioviana@hotmail.com>
2021-07-20 14:20:47 -0300

    Endpoint de Cadastro Feito

---
+++ src/data/users/createUser.ts
@@ -0,0 +1,10 @@
+import { userCreator } from "../../model/users"
+import { connection } from "../connection"
+
+
+export const createUser = async (user: userCreator) : Promise<void> => {
+
+    await connection.raw(`INSERT INTO MC_Users VALUES
+    ("${user.id}", "${user.nickname}", "", "${user.email}", "${user.phone}", "${user.password}", 
+    "${user.role}", CURDATE(), CURDATE())`)
+}
